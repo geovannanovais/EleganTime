06b24614d1010e4c1cf50ba58c606ef111a5a668
geovannanovais <geovannasousano@gmail.com>
2024-09-04 19:24:53 -0300

    Atualizando script BD

--- script do banco de dados.sql
+++ script do banco de dados.sql
@@ -9,18 +9,15 @@ CREATE TABLE Usuario (
     idUsuario INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     cpf BIGINT NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE, -- email tem que ser unico cadastrado
     grupo ENUM('Administrador', 'Estoquista') NOT NULL,
-    senha INT NOT NULL,
+    senha VARCHAR(100) NOT NULL CHECK (LENGTH(senha) >= 5), -- senha tem que ser maior que 5
     condicaoDoUsuario BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 -- Inserindo um registro na tabela Usuario
 INSERT INTO Usuario (nome, cpf, email, grupo, senha, condicaoDoUsuario) 
-VALUES ('Admin', 12345678900, 'admin@admin.com.br', 'Administrador', 1234, TRUE);
+VALUES ('Admin', 12345678900, 'admin@admin.com.br', 'Administrador', '12345', TRUE);
 
 -- Selecionando todos os registros da tabela Usuario
 SELECT * FROM Usuario;
-
--- Excluindo o banco de dados (opcional, se você quiser remover tudo após testar)
-DROP DATABASE elegantime;
